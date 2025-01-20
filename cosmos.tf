09edfd362688953a13c7a6980da18befe5879983
Raghu <Raghuveer.uf25@gmail.com>
2025-01-19 22:29:58 -0600

    Update cosmos.tf

--- cosmos.tf
+++ cosmos.tf
@@ -2,137 +2,41 @@ provider "azurerm" {
   features {}
 }
 
-# Variables
-variable "location" {
-  default = "eastus"
+resource "azurerm_resource_group" "rg" {
+  name     = "testrg"
+  location = var.region
 }
 
-variable "resource_group_name" {
-  default = "rg-cosmosdb-pgsql"
+resource "random_password" "cosmosdb_postgresql_passwords" {
+  length      = 24
+  min_upper   = 6
+  min_lower   = 4
+  min_numeric = 6
+  special     = false
 }
 
-variable "key_vault_name" {
-  default = "kv-cosmosdb-pgsql"
+data "azurerm_cosmosdb_postgresql_cluster" "primary" {
+  name                = var.primary_cluster_name
+  resource_group_name = var.primary_resource_group_name
+  count               = var.is_primary ? 0 : 1
 }
 
-variable "cosmosdb_name" {
-  default = "cosmosdb-pgsql"
-}
+module "cosmosdb_postgresql_cluster" {
+  source                           = "./modules/cosmosdb_postgresql_cluster"
+  name                             = var.cluster_name
+  resource_group_name              = azurerm_resource_group.rg.name
+  location                         = var.region
+  password                         = random_password.cosmosdb_postgresql_passwords.result
+  coordinator_storage_quota_in_mb  = 262144
+  coordinator_vcore_count          = 2
+  node_count                       = 2
+  node_storage_quota_in_mb         = 262144
+  node_vcores                      = 2
+  node_server_edition              = "MemoryOptimized"
+  coordinator_public_ip_access_enabled = var.is_primary ? false : true
+  citus_version                        = "12.1"
+  sql_version                          = "16"
 
-variable "vnet_name" {
-  default = "vnet-cosmosdb"
-}
-
-variable "subnet_name" {
-  default = "subnet-cosmosdb"
-}
-
-# Resource Group
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-# Virtual Network
-resource "azurerm_virtual_network" "main" {
-  name                = var.vnet_name
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-# Subnet for Private Link
-resource "azurerm_subnet" "main" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-  enforce_private_link_endpoint_network_policies = true
-}
-
-# Private DNS Zone (Optional - Only if DNS required)
-resource "azurerm_private_dns_zone" "main" {
-  name                = "privatelink.postgres.database.azure.com"
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-# Private DNS Link (Optional)
-resource "azurerm_private_dns_zone_virtual_network_link" "main" {
-  name                  = "vnet-link"
-  resource_group_name   = azurerm_resource_group.main.name
-  private_dns_zone_name = azurerm_private_dns_zone.main.name
-  virtual_network_id    = azurerm_virtual_network.main.id
-}
-
-# Key Vault
-resource "azurerm_key_vault" "main" {
-  name                        = var.key_vault_name
-  location                    = azurerm_resource_group.main.location
-  resource_group_name         = azurerm_resource_group.main.name
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  sku_name                    = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    secret_permissions = ["get", "set", "delete", "list"]
-  }
-}
-
-# Store Admin Password in Key Vault
-resource "azurerm_key_vault_secret" "admin_password" {
-  name         = "cosmosdb-admin-password"
-  value        = random_password.admin_password.result
-  key_vault_id = azurerm_key_vault.main.id
-}
-
-# Generate Random Password
-resource "random_password" "admin_password" {
-  length  = 16
-  special = true
-}
-
-# Fetch the password dynamically from Key Vault
-data "azurerm_key_vault_secret" "admin_password" {
-  name         = azurerm_key_vault_secret.admin_password.name
-  key_vault_id = azurerm_key_vault.main.id
-}
-
-# CosmosDB for PostgreSQL
-resource "azurerm_cosmosdb_postgresql_cluster" "main" {
-  name                = var.cosmosdb_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  administrator_login   = "adminuser"
-  administrator_password = data.azurerm_key_vault_secret.admin_password.value
-  sku_name              = "Standard_D4s_v3"
-  postgres_version      = "15"
-
-  storage_mb            = 102400
-  backup_retention_days = 7
-  geo_redundant_backup_enabled = false
-
-  network_configuration {
-    subnet_id = azurerm_subnet.main.id
-  }
-
-  tags = {
-    environment = "production"
-  }
-}
-
-# Private Endpoint
-resource "azurerm_private_endpoint" "main" {
-  name                = "pe-cosmosdb"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  subnet_id           = azurerm_subnet.main.id
-
-  private_service_connection {
-    name                           = "psc-cosmosdb"
-    private_connection_resource_id = azurerm_cosmosdb_postgresql_cluster.main.id
-    is_manual_connection           = false
-  }
+  source_location      = var.is_primary ? null : data.azurerm_cosmosdb_postgresql_cluster.primary[0].location
+  source_resource_id   = var.is_primary ? null : data.azurerm_cosmosdb_postgresql_cluster.primary[0].id
 }
